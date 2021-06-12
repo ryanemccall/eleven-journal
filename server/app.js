79bdffa48dbbd281724cc9243df17891efe8f399
Ryan Modrall <ryanemccall20@gmail.com>
2021-06-12 09:56:51 -0400

    Uploading the ElevenJournal Server information

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const Express = require("express");
+const app = Express();
+const dbConnection = require("./db");
+
+const controllers = require("./controllers");
+
+app.use(Express.json());
+
+app.use("/journal", controllers.journalController);
+app.use("/user", controllers.userController);
+
+dbConnection.authenticate()
+    .then(() => dbConnection.sync())
+    .then(() => {
+        app.listen(3000, () => {
+            console.log(`[Server]: App is listening on 3000.`);
+    });
+})
+.catch((err) => {
+    console.log(`[Server]: Server crashed. Error = ${err}`);
+});
---
+++ server/controllers/usercontroller.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { UserModel } = require("../models");
+
+router.post("/register", async (req, res) => {
+
+    let { email, password } = req.body.user;
+
+    UserModel.create({
+        email,
+        password
+    })
+});
+
+module.exports = router;
